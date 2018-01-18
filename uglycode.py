3278f02a521c5412ab9181bc1b2bce0967b7f7ac
david Raftrey <dwraftrey@lbl.gov>
2018-01-18 09:37:26 -0800

    working on two particle simulations

--- uglycode.py
+++ uglycode.py
@@ -20,9 +20,9 @@ top.pline2 = " " + gitversion()
 top.runmaker = "Arun Persaud (apersaud@lbl.gov)"
 
 # Parameters available for scans
-gap = 500*um
-Vesq = 548.0
-top.dt = 5e-11
+gap = 500*um # distance between grounded and potential RF plates
+Vesq = 548.0 # voltage of ESQs
+top.dt = 5e-11 # time beam is between ground and potential???
 
 # --- Invoke setup routine for the plotting
 setup(prefix="esq-V{}-gap-{}um".format(int(Vesq), int(gap*1e6)))
@@ -32,7 +32,16 @@ emittingradius = 40*um
 ibeaminit = 20e-6
 ekininit = 40e3
 
-ions = Species(type=Xenon, charge_state=1, name='Xe')
+
+# selectedIons = Species(type=Xenon, charge_state=1, name='Xe', color=green)
+# rejectedIons = Species(type=Xenon, charge_state=1, name='Xe', color=red)
+
+selectedIons = Species(type=Phosphorus, charge_state=1, name='P', color=green)
+rejectedIons = Species(type=Sulfur, charge_state=1, name='S', color=red)
+
+ # number of rf cells
+ #each cell has 2 acceleration stages
+rfCount = 30
 
 top.a0 = emittingradius
 top.b0 = emittingradius
@@ -42,8 +51,7 @@ top.vbeam = .0e0
 top.emit = 0.77782e-6
 top.ibeam = ibeaminit
 top.ekin = ekininit
-top.aion = ions.type.A
-top.zion = ions.charge_state
+top.zion = selectedIons.charge_state
 #top.vthz = 0.0
 top.lrelativ = False
 derivqty()
@@ -137,21 +145,26 @@ freq = 100e6
 
 def gen_volt(toffset=0):
     def RFvoltage(time):
-        return Vmax*np.sin(2*np.pi*freq*(time-toffset))
+        return Vmax*np.sin(2*np.pi*freq*(time+toffset))
     return RFvoltage
 
-toffsets = [3.5e-9, 0.0e-9, 5.5e-9, 0.5e-9, 4.5e-9, 8.5e-9]
+#origional time offsets for Xe
+#toffsets = [3.5e-9, 0.0e-9, 5.5e-9, 0.5e-9, 4.5e-9, 8.5e-9]
+
+toffsets = [0] * rfCount
 Ekin = ekininit
-rfgap = np.sqrt(2*Ekin*ions.charge/ions.mass)/freq/2-0.5*mm
-rfgaps = [rfgap+0.5*mm, rfgap+0.144*mm, rfgap+0.146*mm,
-          rfgap+0.375*mm, rfgap+0.2992*mm, rfgap+0.6986*mm]
+rfgap = np.sqrt(2*Ekin*selectedIons.charge/selectedIons.mass)/freq/2-0.5*mm
+rfgaps = [rfgap] * rfCount
+
+# Origional manualy adjusted gaps for Xe
+# rfgaps = [rfgap+0.5*mm, rfgap+0.144*mm, rfgap+0.146*mm,
+#           rfgap+0.375*mm, rfgap+0.2992*mm, rfgap+0.6986*mm]
+
+
 Vpos = []
 
-for i, toffset, rfgap in zip(range(6), toffsets, rfgaps):
-    Ekin +=
-
-
-    * 0.8 * Vmax
+for i, toffset, rfgap in zip(range(rfCount), toffsets, rfgaps):
+    Ekin += 2 * 0.8 * Vmax
     RF = RF_stack2(condid=[ID_RF, ID_RF+1, ID_RF+2, ID_RF+3],
                    rfgap=rfgap, voltage=gen_volt(toffset))
     Vpos.append(geometry.pos)
@@ -179,7 +192,7 @@ print("total length", geometry.pos)
 #scraper = ParticleScraper(ESQs +  RFs)
 conductors = sum(ESQs) + sum(RFs)
 
-velo = np.sqrt(2*ekininit*ions.charge/ions.mass)
+velo = np.sqrt(2*ekininit*selectedIons.charge/selectedIons.mass)
 length = geometry.pos
 tmax = length/velo
 zrunmax = length
@@ -216,7 +229,7 @@ while (top.time < tmax and zmax < zrunmax):
     step(10)
 
     Volts = []
-    for i, t in zip(range(6), toffsets):
+    for i, t in zip(range(rfCount), toffsets):
         func = gen_volt(t)
         Volts.append(func(top.time))
 
@@ -230,44 +243,69 @@ while (top.time < tmax and zmax < zrunmax):
     top.pline1 = tmp
 
     # inject only for 1 ns, so that we can get onto the rising edge of the RF
-    if 0 < top.time < 1e-9:
-        top.inject = 1
+    if 0 < top.time < 10e-9:
+        top.finject[0,selectedIons.jslist[0]] = 1 #?
+        #top.finject[0,rejectedIons.jslist[0]] = 1 #?
+        #top.inject = 1
     else:
         top.inject = 0
 
-    Z = ions.getz()
+    #which species to follow
+    Z = selectedIons.getz()
     if Z.mean() > zmid:
-        top.vbeamfrm = ions.getvz().mean()
+        top.vbeamfrm = selectedIons.getvz().mean()
         solver.gridmode = 0
 
     zmin = top.zbeam+w3d.zmmin
     zmax = top.zbeam+w3d.zmmax
 
     # create some plots
-    KE = ions.getke()
+    KE = selectedIons.getke()
     if len(KE) > 0:
-        ions.ppzke(color=red)
+        selectedIons.ppzke(color=red)
         KEmin, KEmax = KE.min(), KE.max()
         while KEmax-KEmin > deltaKE:
             deltaKE += 10e3
         ylimits(0.95*KEmin, 0.95*KEmin+deltaKE)
         fma()
+
     pfxy(iz=w3d.nz//2, fill=0, filled=1, plotselfe=2, comp='E', titles=0, cmin=0, cmax=5e6*Vesq/125)
     limits(-w3d.xmmax, w3d.xmmax)
     ylimits(-w3d.ymmax, w3d.ymmax)
     ptitles("Geometry and Fields", "X [m]", "Y [m]", "")
     fma()
+
+    # moveie plot
     pfzx(fill=1, filled=1, plotselfe=2, comp='z', titles=0, cmin=0, cmax=5e6)
-    ions.ppzx(color=red, titles=0)
+    selectedIons.ppzx(color=green, titles=0)
+    rejectedIons.ppzx(color=red, titles=0)
     ptitles("Partcles and Fields", "Z [m]", "X [m]", "")
     limits(zmin, zmax)
     fma()
-    ions.ppxy(color=red, titles=0)
+
+    selectedIons.ppxy(color=red, titles=0)
     limits(-R, R)
     ylimits(-R, R)
     plg(Y, X, type="dash")
     fma()
+
+    Z = selectedIons.getz()
+    if Z.mean() > zmid:
+        top.vbeamfrm = selectedIons.getvz().mean()
+        solver.gridmode = 0
+
+    zmin = top.zbeam+w3d.zmmin
+    zmax = top.zbeam+w3d.zmmax
+
+    pfzx(fill=1, filled=1, plotselfe=2, comp='z', titles=0, cmin=0, cmax=5e6)
+    selectedIons.ppzx(color=green, titles=0)
+    rejectedIons.ppzx(color=red, titles=0)
+    ptitles("Partcles and Fields", "Z [m]", "X [m]", "")
+    limits(zmin, zmax)
+    fma()
+
     refresh()
+
 
 # plot particle vs time
 # hpepsnxz()
@@ -275,6 +313,7 @@ while (top.time < tmax and zmax < zrunmax):
 # hpepsnyz()
 # hpepsnx()
 # fma()
+
 hpepsny()
 fma()
 hpepsnz()
@@ -293,26 +332,30 @@ hprrms(color=green, titles=0)
 ptitles("X(red), Y(blue), R(green)", "Z [m]", "X/Y/R [m]", "")
 
 fma()
-hppnum()
 
+# plot number of particles over time
+hppnum()
 fma()
+
 # hplinechg()
 # fma()
 
+
+
 # save history information, so that we can plot all cells in one plot
 t = np.trim_zeros(top.thist, 'b')
-hepsny = ions.hepsny[0]
-hepsnz = ions.hepsnz[0]
-hep6d = ions.hepsx[0] * ions.hepsy[0] * ions.hepsz[0]
-hekinz = 1e-6*0.5*top.aion*amu*ions.hvzbar[0]**2/jperev
+hepsny = selectedIons.hepsny[0]
+hepsnz = selectedIons.hepsnz[0]
+hep6d = selectedIons.hepsx[0] * selectedIons.hepsy[0] * selectedIons.hepsz[0]
+hekinz = 1e-6*0.5*top.aion*amu*selectedIons.hvzbar[0]**2/jperev
 
-u = ions.hvxbar[0]**2 + ions.hvybar[0]**2 + ions.hvzbar[0]**2
+u = selectedIons.hvxbar[0]**2 + selectedIons.hvybar[0]**2 + selectedIons.hvzbar[0]**2
 hekin = 1e-6 * 0.5*top.aion*amu*u/jperev
 
-hxrms = ions.hxrms[0]
-hyrms = ions.hyrms[0]
-hrrms = ions.hrrms[0]
-hpnum = ions.hpnum[0]
+hxrms = selectedIons.hxrms[0]
+hyrms = selectedIons.hyrms[0]
+hrrms = selectedIons.hrrms[0]
+hpnum = selectedIons.hpnum[0]
 
 for i in (t, hepsny, hepsnz, hep6d, hekinz, hekin, hxrms, hyrms, hrrms, hpnum):
     print(len(i))
