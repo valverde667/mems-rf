f5559c2c718185cd0d42978d868ac29a7be44079
Maddy Garske <MWGarske@lbl.gov>
2019-07-22 14:54:58 -0700

    geometry up to date for PCB ESQ radius of electrodes 8/7*aperture radius, offset for electrodes at proper distance, old geometries removed more comments added to describe code

--- geometry.py
+++ geometry.py
@@ -1,4 +1,4 @@
-"""Module to keep track of different geometries used in our project"""
+"""Module to keep track of different geometries used in our current project"""
 import warp as wp
 # universial dimensions
 
@@ -8,17 +8,14 @@ framewidth = 150*wp.um
 
 # wafer dimenions
 # SOI
-ESQ_wafer_body = 678*wp.um #should this be the same as the RF wafer length?
-ESQ_wafer_box = 2*wp.um #copper plating on the silicon ? (1um on both sides?)
-ESQ_wafer_si = 20*wp.um #silicon thickness? But it does not seem this slim -MWG
-ESQ_wafer_length = ESQ_wafer_body + ESQ_wafer_box + ESQ_wafer_si
+ESQ_wafer_length = 625*wp.um #
 # RF
 RF_thickness = 625*wp.um
 RF_gap = 2000*wp.um #acceleration gap
 # esq
 ESQ_gap = .7*wp.mm #how is this calculated?
 # globals
-pos = 0 #This is not being looked at
+pos = 0
 mid_gap = []
 
 def Gap(dist=500*wp.um): #why is this 500 um, just a default value?
@@ -31,20 +28,16 @@ def Gap(dist=500*wp.um): #why is this 500 um, just a default value?
 def ESQ(voltage, condid):
     """Simple ESQ wafer
 
-    Use 3 cylinders in Z to make up a single electrode.
+    Use 4 cylinders in Z to make up a single electrode.
     Add to current position(pos) and use two condids [a,b] for the electrods.
 
     Bias these +-+- with voltage V.
 
     """
     global pos
-    scaling_factor = 10  
-    R1 = 1*wp.mm #96*wp.um*scaling_factor  # center cylinder
-    R2 = 75*wp.um*scaling_factor  # outside cylinders
+    R1 = .5*(8/7)*wp.mm  #radius of electrodes
 
-    X = -337*wp.um*scaling_factor  # X offset for outer electrodes
-    Y = 125*wp.um*scaling_factor  # +-Y offset for outer electrodes
-    XX = 2*wp.mm  #-187*wp.um*scaling_factor  # X offset for inner electrode
+    X = .5*(15/7)*wp.mm#2*wp.mm  #X offset for electrodes
 
     print("--- ESQ starts at: ", pos)
     print("--- ESQ voltage: ", voltage)
@@ -54,34 +47,20 @@ def ESQ(voltage, condid):
         """create a single element, rotated X degrees"""
 
         if rotation == 0:
-            xcent1, ycent1 = X, Y
-            xcent2, ycent2 = X, -Y
-            xcent3, ycent3 = XX, 0
+            xcent1, ycent1 = X, 0
         elif rotation == 90:
-            xcent1, ycent1 = Y, -X
-            xcent2, ycent2 = -Y, -X
-            xcent3, ycent3 = 0, -XX
+            xcent1, ycent1 = 0, -X
         elif rotation == 180:
-            xcent1, ycent1 = -X, Y
-            xcent2, ycent2 = -X, -Y
-            xcent3, ycent3 = -XX, 0
+            xcent1, ycent1 = -X, 0
         elif rotation == 270:
-            xcent1, ycent1 = Y, X
-            xcent2, ycent2 = -Y, X
-            xcent3, ycent3 = 0, XX
+            xcent1, ycent1 = 0, X
         else:
             print("wrong rotation value")
-
-        electrode1 = wp.ZCylinder(radius=R2, length=ESQ_wafer_length, voltage=voltage,
+        
+        electrode = wp.ZCylinder(radius=R1, length=ESQ_wafer_length, voltage=voltage,
                                xcent=xcent1, ycent=ycent1, zcent=zcenter,
                                condid=condid)
-        electrode2 = wp.ZCylinder(radius=R2, length=ESQ_wafer_length, voltage=voltage,
-                               xcent=xcent2, ycent=ycent2, zcent=zcenter,
-                               condid=condid)
-        electrode3 = wp.ZCylinder(radius=R1, length=ESQ_wafer_length, voltage=voltage,
-                               xcent=xcent3, ycent=ycent3, zcent=zcenter,
-                               condid=condid)
-        return electrode3#electrode1 + electrode2 + electrode3
+        return electrode
 
     condidA, condidB = condid
 
@@ -95,32 +74,6 @@ def ESQ(voltage, condid):
         pos_voltage = voltage
         neg_voltage = -voltage
 
-    bodycenter = zcenter - 0.5*ESQ_wafer_length + ESQ_wafer_body/2  # assume body of SOI is on the left
-    Frame1 = wp.Box(framelength, framelength, ESQ_wafer_body,
-                 zcent=bodycenter, voltage=pos_voltage, condid=condidA)
-    Frame2 = wp.Box(framelength-2*framewidth, framelength-2*framewidth, ESQ_wafer_body*1.1,
-                 zcent=bodycenter, voltage=pos_voltage, condid=condidA)
-    InnerBox1 = wp.Box(framelength/2+X, 2*(Y+R2), ESQ_wafer_body,
-                    xcent=-framelength/2.+(framelength/2.+X)/2.,
-                    zcent=bodycenter, voltage=pos_voltage, condid=condidA)
-    InnerBox2 = wp.Box(framelength/2+X, 2*(Y+R2), ESQ_wafer_body,
-                    xcent=framelength/2.-(framelength/2.+X)/2.,
-                    zcent=bodycenter, voltage=pos_voltage, condid=condidA)
-    FrameA = (Frame1-Frame2) + InnerBox1 + InnerBox2
-
-    SOIcenter = zcenter + 0.5*ESQ_wafer_length - ESQ_wafer_si/2  # assume body of SOI is on the left
-    Frame1 = wp.Box(framelength, framelength, ESQ_wafer_si,
-                 zcent=SOIcenter, voltage=neg_voltage, condid=condidB)
-    Frame2 = wp.Box(framelength-2*framewidth, framelength-2*framewidth, 2*ESQ_wafer_si,
-                 zcent=SOIcenter, voltage=neg_voltage, condid=condidB)
-    InnerBox1 = wp.Box(2*(Y+R2), framelength/2.+X, ESQ_wafer_si,
-                    ycent=-framelength/2.+(framelength/2.+X)/2.,
-                    zcent=SOIcenter, voltage=neg_voltage, condid=condidB)
-    InnerBox2 = wp.Box(2*(Y+R2), framelength/2.+X, ESQ_wafer_si,
-                    ycent=framelength/2.-(framelength/2.+X)/2.,
-                    zcent=SOIcenter, voltage=neg_voltage, condid=condidB)
-    FrameB = (Frame1-Frame2) + InnerBox1 + InnerBox2
-
     electrodeA = element(voltage=pos_voltage, condid=condidA, rotation=0)
     electrodeB = element(voltage=neg_voltage, condid=condidB, rotation=90)
     electrodeC = element(voltage=pos_voltage, condid=condidA, rotation=180)
@@ -129,7 +82,7 @@ def ESQ(voltage, condid):
     pos += ESQ_wafer_length
     print("--- ESQ ends at: ", pos)
 
-    return electrodeA + electrodeB + electrodeC + electrodeD# + FrameA + FrameB
+    return electrodeA + electrodeB + electrodeC + electrodeD
 
 def RF_stack3(condid, betalambda_half=200*wp.um, gap=RF_gap, voltage=0):
     """4 wafers rf stack with 2 acceleration stages
@@ -147,7 +100,7 @@ def RF_stack3(condid, betalambda_half=200*wp.um, gap=RF_gap, voltage=0):
     wafercenter = pos + 0.5*RF_thickness
     Frame = wp.Box(framelength, framelength, RF_thickness, voltage=0,
                 zcent=wafercenter, condid=condidA)
-                #why is there 50um being added to the rf thickness below???
+                #why is there 50um being added to the rf thickness below?
     Beam = wp.ZCylinder(r_beam, RF_thickness + 50*wp.um, zcent=wafercenter, voltage=0, condid=condidA)
     Ground1 = Frame-Beam
     pos += RF_thickness + gap
@@ -176,25 +129,3 @@ def RF_stack3(condid, betalambda_half=200*wp.um, gap=RF_gap, voltage=0):
     pos += RF_thickness
 
     return Ground1 + body1 + Ground2
-
-
-def Aperture(voltage, condid, width=10*wp.um):
-    """A simple thin wafer with a whole for the beam"""
-
-    global pos
-    print("--- Aperture starts at: ", pos)
-    print("--- Aperture voltage: ", voltage)
-    print("--- Aperture width: ", width)
-
-    r_beam = 90*wp.um
-    bodycenter = pos + 0.5*width
-
-    Frame = wp.Box(framelength, framelength, width,
-                zcent=bodycenter, voltage=voltage, condid=condid)
-    Beam = wp.ZCylinder(r_beam, 2*width, zcent=bodycenter, voltage=voltage, condid=condid)
-    body = Frame-Beam
-
-    pos += width
-    print("--- Aperture ends at: ", pos)
-
-    return body
