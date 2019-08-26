1aec02528d3a25ea0f8dc520494e8851b42f3484
Maddy Garske <MWGarske@lbl.gov>
2019-08-26 16:37:57 -0700

    added options to usage, tried adding a check for valid commands

--- auto_scan.py
+++ auto_scan.py
@@ -1,23 +1,29 @@
 #x!/usr/local/bin/python3
 """
     Usage:
-    auto_scan_try_2 [options] <parameter> <start> <end> <N>
+    auto_scan [options] <parameter> <start> <end> <N>
     
     Options:
+    
+    <parameter>
+        bunch_length
+        numRF
+        rf_voltage
+        esq_voltage
+        fraction
+        species_mass
+        ekininit
+        freq
+        emit
+        diva
     
     """
 
 import numpy as np
 import os
 import math
-import datetime
-import matplotlib.pyplot as plt
-import sys
 from docopt import docopt #to use commands via commandline
-import array as arr
 from pathlib import Path
-
-import glob
 
 commands = docopt(__doc__)
 print(commands)
@@ -27,13 +33,15 @@ start = commands['<start>']
 end = commands['<end>']
 iterations = commands['<N>']
 
-parameter_list = np.arange(float(start), float(end), float(iterations)) #this is working! just stops before last entered number in end
+"""if not parameter_name == 'bunch_length' or 'numRF' or 'rf_voltage' or 'esq_voltage'  or    'fraction' or 'species_mass' or 'ekininit' or 'freq' or 'emit' or 'diva':
+    print("Please enter a valid parameter")
+    exit()
+else:"""
 
-#print(parameter_list) #check if list has proper numbers
+parameter_list = np.arange(float(start), float(end), float(iterations)) #stops before last entered number in end
+
+    #print(parameter_list) #check if list has proper numbers
 
 for i, x in enumerate(parameter_list):
     print(f"........This is the start of parameter change {i+1} for {parameter_name}........")
     os.system(f"python3 single-species-simulation.py --{parameter_name}={x}e-9")
-
-
-
