e9a3325bbafa09e76c4f796f6a619441ca79e873
Nick Valverde <valverde667@gmail.com>
2020-08-07 19:31:52 -0400

    Create ESQ class. Load doublet on unit cell mesh. Initialize plots for analysis.

--- valverde/Geometry/esq_field_testing.py
+++ valverde/Geometry/esq_field_testing.py
@@ -5,7 +5,7 @@ import warp as wp
 
 kV = wp.kV
 mm = wp.mm
-
+um = 1e-6
 
 wp.w3d.xmmin = -2 * mm
 wp.w3d.xmmax = 2 * mm
@@ -16,14 +16,47 @@ wp.w3d.ymmax = 2 * mm
 wp.w3d.ny = 100
 
 wp.w3d.zmmin = 0
-wp.w3d.zmmax = 10 * mm
-wp.w3d.nz = 200
+wp.w3d.zmmax = 5 * mm
+wp.w3d.nz = 300
 
 solver = wp.MRBlock3D()
 wp.registersolver(solver)
 
 
-def esq(voltage, radius=0.5 * mm, center=1 * mm, zc=3 * mm, length=2 * mm):
+class ESQ:
+    def __init__(self, radius=0.5 * mm, zc=3 * mm, length=625 * um):
+
+        self.radius = radius
+        self.zc = zc
+        self.length = length
+
+    def pole(self, voltage, xcent, ycent):
+        conductor = wp.ZCylinder(
+            voltage=voltage,
+            xcent=xcent,
+            ycent=ycent,
+            zcent=self.zc,
+            radius=self.radius,
+            length=self.length,
+        )
+        return conductor
+
+    def generate(self, voltage, xcent, ycent, data=False):
+        top = self.pole(voltage=voltage, xcent=0, ycent=ycent)
+        bottom = self.pole(voltage=voltage, xcent=0, ycent=-ycent)
+        left = self.pole(voltage=-voltage, xcent=-xcent, ycent=0)
+        right = self.pole(voltage=-voltage, xcent=xcent, ycent=0)
+
+        components = [top, bottom, left, right]
+        conductor = top + bottom + left + right
+
+        if data == False:
+            return conductor
+        else:
+            return conductor, components
+
+
+def esq(voltage, radius=0.5 * mm, center=1 * mm, zc=3 * mm, length=625 * um):
     # --Create top pole
     pole1 = wp.ZCylinder(
         radius=radius, length=length, voltage=voltage, xcent=0, ycent=center, zcent=zc,
@@ -57,8 +90,23 @@ def esq(voltage, radius=0.5 * mm, center=1 * mm, zc=3 * mm, length=2 * mm):
 
 
 voltage = 1 * kV
-quad = esq(voltage)
-wp.installconductors(quad)
+xycent = 1 * mm
+separation = 0.7 * mm
+zc = 3 * mm
+length = 625 * um
+conductor = ESQ()
+quadclass1, components = conductor.generate(
+    voltage=voltage, xcent=xycent, ycent=xycent, data=True
+)
+
+conductor1 = ESQ(zc=zc + length / 2 + separation + length / 2)
+quadclass2, components2 = conductor1.generate(
+    voltage=-voltage, xcent=xycent, ycent=xycent, data=True
+)
+
+# quad = esq(voltage)
+
+wp.installconductors(quadclass1, quadclass2)
 wp.generate()
 
 x, y, z = wp.w3d.xmesh, wp.w3d.ymesh, wp.w3d.zmesh
@@ -67,27 +115,69 @@ xzeroindex = np.transpose(np.where(x == 0))[0, 0]
 yzeroindex = np.transpose(np.where(y == 0))[0, 0]
 zcenterindex = np.transpose(np.where(z == 3 * mm))[0, 0]
 
+# wp.setup()
+# quadclass1.drawzx(filled=True)
+# wp.limits(0, 10 * mm, -2 * mm, 2 * mm)
+# wp.fma()
+# quadclass1.drawxy(filled=True)
+# wp.fma()
+# wp.limits(-2 * mm, 2 * mm, -2 * mm, 2 * mm)
+# wp.pfxy(iz=yzeroindex,)
+# wp.fma()
+# wp.pfzx(fill=1, filled=1)
+# wp.fma()
+
+# wp.setup()
+# wp.limits(0, 10 * mm, -2 * mm, 2 * mm)
+# quadclass2.drawzx(filled=True)
+# wp.fma()
+# quadclass2.drawxy(filled=True)
+# wp.fma()
+# wp.pfxy(iz=yzeroindex,)
+# wp.fma()
+# wp.pfzx(fill=1, filled=1)
+# wp.fma()
+
 wp.setup()
-quad.drawzx(filled=True)
+quadclass1.drawzx(filled=True)
+quadclass2.drawzx(filled=True)
 wp.limits(0, 10 * mm, -2 * mm, 2 * mm)
 wp.fma()
-quad.drawxy(filled=True)
+quadclass1.drawxy(filled=True)
 wp.fma()
-wp.limits(-2 * mm, 2 * mm, -2 * mm, 2 * mm)
 wp.pfxy(iz=yzeroindex,)
 wp.fma()
-wp.pfzx(fill=1, filled=1)
+wp.pfzx()
 wp.fma()
 
-raise Exception()
-
-
+phi = wp.getphi()
 phixy = wp.getphi()[:, :, zcenterindex]
+Ex = wp.getselfe(comp="x")
+gradex = Ex[xzeroindex + 1, yzeroindex, :] / wp.w3d.dx
 
 fig, ax = plt.subplots()
-ax.set_xlabel("x [mm]")
-ax.set_ylabel("y [mm]")
-X, Y = np.meshgrid(x, y)
-cont = ax.contour(X / mm, Y / mm, phixy, levels=50)
-cb = fig.colorbar(cont)
+ax.set_xlabel("z [mm]")
+ax.set_ylabel(r"$E_x(dx, 0, z)$/dx [V mm$^{-2}$]")
+ax.set_title(r"$E_x$ Gradient One Grid-cell Off-axis vs z")
+ax.scatter(z / mm, gradex / mm / mm, s=1.2)
+
+# add ESQ
+esq1left = zc - length / 2
+esq1right = zc + length / 2
+
+esq2left = zc + length / 2 + separation
+esq2right = zc + length / 2 + separation + length
+ax.axvline(x=esq1left / mm, c="b", lw=0.8, ls="--", label="First ESQ")
+ax.axvline(x=esq1right / mm, c="b", lw=0.8, ls="--")
+ax.axvline(x=esq2left / mm, c="r", lw=0.8, ls="--", label="Second ESQ")
+ax.axvline(x=esq2right / mm, c="r", lw=0.8, ls="--")
+plt.legend()
 plt.show()
+
+# fig, ax = plt.subplots()
+# ax.set_xlabel("x [mm]")
+# ax.set_ylabel("y [mm]")
+# X, Y = np.meshgrid(x, y)
+# cont = ax.contour(X / mm, Y / mm, phixy, levels=50)
+# cb = fig.colorbar(cont)
+# plt.show()
