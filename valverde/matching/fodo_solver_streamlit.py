41f201b0d58d31fec72655c95bf72e3dfd44dd57
Nick Valverde <nvalverde@lbl.gov>
2021-01-24 12:20:30 -0700

    Create lattice mesh. Format sidebar for voltages.
    Setting the voltage is more sensical since this is controlled rather than
    kappa.

--- valverde/matching/fodo_solver_streamlit.py
+++ valverde/matching/fodo_solver_streamlit.py
@@ -33,8 +33,11 @@ Q = st.sidebar.number_input(
 emittance = st.sidebar.number_input(
     "Emittance e", 1e-6, 1e-4, param_dict["emittance"], step=4.95e-6, format="%.4e"
 )
-kappa = st.sidebar.number_input(
-    "Focusing Strength k", 0.0, 7e4, 4e4, step=0.5e4, format="%e"
+V1 = st.sidebar.number_input(
+    "Voltage on First ESQ V1", 0.0, 5.5 * kV, step=0.5 * kV, format="%.2e"
+)
+V2 = st.sidebar.number_input(
+    "Voltage on Second ESQ V2", -5.5 * kV, 0.0, step=0.5 * kV, format="%.2e"
 )
 ux = st.sidebar.number_input(
     "x Injection Position", 0.2 * mm, 0.5 * mm, 0.5 * mm, step=0.05 * mm, format="%e"
@@ -49,3 +52,16 @@ vx = st.sidebar.number_input(
 vx = st.sidebar.number_input(
     "x-angle Injection", -5 * mrad, -2 * mrad, -5 * mrad, step=0.5 * mrad, format="%e"
 )
+
+# I will now set up the simulation mesh. The whole mesh will be created. But,
+# in order to see the effects of the beginning drift, the solver will start
+# from the second gap initially. This gives the symmetric cell to start. Then,
+# the solver can be tweaked to start further back essentially adding, in
+# increments of ds, a long pre-drift.
+d = 9.3 * mm
+g = 2 * mm
+lq = 0.695 * mm
+N = 500
+L = 2 * d
+ds = L / N
+s = np.linspace(0, L, N)
