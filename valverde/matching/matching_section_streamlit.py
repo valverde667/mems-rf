c4ab2baf4d5efd252084cbc353ebef4379a30320
Nick Valverde <nvalverde@lbl.gov>
2021-03-01 18:53:43 -0700

    Initialize streamlit script for matching section.

---
+++ valverde/matching/matching_section_streamlit.py
@@ -0,0 +1,6 @@
+"""Script to investigate matching section before accelerating lattice. The
+2018 paper found suitable solutions from six voltage settings and a FDDFFD
+setup. The six voltages were in Volts: 551, -510, -512, 359, 352, -136.
+Executing <streamlit run match_section_streamlit.py> in the terminal will open
+a browser window where the voltages can be set and the solution to the KV
+envelope equation will be plotted in this matching region."""
