f6433e3f39c9cab075cc35b368ff9a0911a2d896
Nick Valverde <nvalverde@lbl.gov>
2022-02-28 12:23:08 -0800

    working script to load acceleration gaps and collect on-axis fields.

--- valverde/gap_field_function.py
+++ valverde/gap_field_function.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import scipy
+import scipy.constants as SC
 from scipy.optimize import curve_fit
 from scipy.special import jv
 import matplotlib.pyplot as plt
@@ -12,19 +13,43 @@ import pdb
 import warp as wp
 from warp.utils.timedependentvoltage import TimeVoltage
 
-# Useful constants to define for units and such
-mm = 1e-3
-kV = 1e3
-keV = 1e3
+# different particle masses in eV
+# amu in eV
+amu = SC.physical_constants["atomic mass constant energy equivalent in MeV"][0] * 1e6
+
+Ar_mass = 39.948 * amu
+He_mass = 4 * amu
+p_mass = amu
+kV = 1000
+keV = 1000
 MHz = 1e6
-us = 1e-6
-ns = 1e-9
+cm = 1e-2
+mm = 1e-3
+ns = 1e-9  # nanoseconds
+twopi = 2 * np.pi
+
+
+def beta(E, mass=Ar_mass, q=1, nonrel=True):
+    """Velocity of a particle with energy E."""
+    if nonrel:
+        beta = np.sqrt(2 * E / mass)
+    else:
+        gamma = (E + mass) / mass
+        beta = np.sqrt(1 - 1 / gamma / gamma)
+
+    return beta
+
+
+def calc_pires(energy, freq, mass=Ar_mass, q=1):
+    """RF resonance condition in pi-mode"""
+    beta_lambda = beta(energy, mass=mass, q=q) * SC.c / freq
+    return beta_lambda / 2
 
 
 def gap_voltage(t):
     """"Sinusoidal function of the gap voltage"""
 
-    v = 3.5 * kV * np.cos(2 * np.pi * 13.6 * MHz * t)
+    v = 7 * kV * np.cos(2 * np.pi * 13.6 * MHz * t)
 
     return v
 
@@ -32,10 +57,66 @@ def gap_voltage(t):
 def neg_gap_voltage(t):
     """"Sinusoidal function of the gap voltage"""
 
-    v = -3.5 * kV * np.cos(2 * np.pi * 13.6 * MHz * t)
+    v = 7 * kV * np.cos(2 * np.pi * 13.6 * MHz * t)
 
     return v
 
+
+def create_gap(
+    cent,
+    left_volt,
+    right_volt,
+    width=2 * mm,
+    length=0.035 * mm,
+    rin=0.55 * mm,
+    rout=0.75 * mm,
+):
+    """Create an acceleration gap consisting of two wafers."""
+
+    left_wafer = wp.Annulus(
+        rmin=rin, rmax=rout, length=length, zcent=cent - length / 2, voltage=left_volt
+    )
+
+    right_wafer = wp.Annulus(
+        rmin=rin, rmax=rout, length=length, zcent=cent + length / 2, voltage=left_volt
+    )
+    gap = left_wafer + right_wafer
+    return gap
+
+
+# Find gap positions. The gap positions will be calculated for 12 gaps giving
+# three lattice periods.
+rout = 0.75 * mm
+rin = 0.50 * mm
+length = 0.035 * mm
+test_length_fact = 3
+gap_width = 2 * mm
+zcenter = abs(0.0 - gap_width / 2)
+f = 13.6 * MHz
+Ng = 12
+Vg = 7 * kV
+E_DC = Vg / gap_width
+dsgn_phase = -np.pi / 2
+gap_cent_dist = []
+Einit = 7 * keV
+
+for i in range(Ng):
+    this_dist = calc_pires(Einit, freq=f)
+    gap_cent_dist.append(this_dist)
+    Einit += 7 * keV * np.cos(dsgn_phase)  # Max acceleration
+
+# Real gap positions are the cumulative sums
+gap_cent_dist = np.array(gap_cent_dist)
+gap_centers = gap_cent_dist.cumsum()
+
+# Shift all gap positions to be centered on the middle lattice period
+cell1 = gap_centers[0:4]
+cell2 = gap_centers[4:8]
+cell3 = gap_centers[8:]
+cell2_center = (cell2[1] + cell2[2]) / 2
+
+shift_gap_centers = gap_centers
+print(shift_gap_centers / mm)
 
 # class Wafer(wp.Assembly):
 #     """A single RF gap comprised of two wafers time varying voltage"""
@@ -122,12 +203,11 @@ wp.w3d.ymmin = -2.5 * mm
 wp.w3d.ymmax = 2.5 * mm
 wp.w3d.ny = 60
 
-wp.w3d.zmmin = -2.5 * mm
-wp.w3d.zmmax = 2.5 * mm
-wp.w3d.nz = 300
-
-wp.top.dt = 1 / 13.6 / MHz / 75
-
+# use gap positioning to find limits on zmesh. Add some spacing at end points.
+# Use enough zpoint to resolve the wafers. In this case, resolve with 2 points.
+wp.w3d.zmmin = shift_gap_centers.min() - 1.5 * cm
+wp.w3d.zmmax = shift_gap_centers.max() + 1.5 * cm
+wp.w3d.nz = round(2 * (wp.w3d.zmmax - wp.w3d.zmmin) / length)
 
 # Add boundary conditions
 wp.w3d.bound0 = wp.dirichlet
@@ -138,65 +218,109 @@ wp.w3d.l4symtry = True
 solver = wp.MRBlock3D()
 wp.registersolver(solver)
 
-rout = 0.75 * mm
-rin = 0.50 * mm
-length = 0.035 * mm
-gap_width = 2 * mm
-zcenter = abs(0.0 - gap_width / 2)
-f = 13.6 * MHz
 
+# Create accleration gaps with correct coordinates and settings. Collect in
+# list and then loop through and install on the mesh.
+conductors = []
+for i, cent in enumerate(shift_gap_centers):
+    if i % 2 == 0:
+        this_cond = create_gap(cent, left_volt=0, right_volt=Vg)
+    else:
+        this_cond = create_gap(cent, left_volt=Vg, right_volt=0)
 
-left_wafer = wp.Annulus(
-    rmin=rin, rmax=rout, length=length, zcent=-zcenter, voltage=gap_voltage
-)
-TimeVoltage(left_wafer, voltfunc=gap_voltage)
-right_wafer = wp.ZAnnulus(
-    rmin=rin, rmax=rout, length=length, zcent=zcenter, voltage=neg_gap_voltage
-)
-TimeVoltage(right_wafer, voltfunc=neg_gap_voltage)
+    conductors.append(this_cond)
 
-wp.installconductor(left_wafer)
-wp.installconductor(right_wafer)
+for cond in conductors:
+    wp.installconductor(cond)
+
+# Perform initial field solve for mesh.
 wp.generate()
+
+# Collect data from the mesh and initialize useful variables.
 z = wp.w3d.zmesh
-steps = 75
+steps = 1
+time = np.zeros(steps)
+Ez_array = np.zeros((steps, len(z)))
+Ez0 = wp.getselfe(comp="z")[0, 0, :]
+phi0 = wp.getphi()[0, 0, :]
 
-# time = np.zeros(steps)
-# Ez_array = np.zeros((steps, len(z)))
-# Ez0 = wp.getselfe(comp='z')[0, 0, :]
-# fig,ax = plt.subplots()
-# phi0 = wp.getphi()[0, 0, :]
-# ax.plot(z/mm, phi0, kV)
-# ax.set_xlabel('z [mm]')
-# ax.set_ylabel("Potential [kV]")
-# ax.axvline(x=-zcenter/mm, c='g', lw=1)
-# ax.axvline(x=zcenter/mm, c='g', lw=1)
-# ax.axhline(y=0, c='k', lw=1)
-# plt.show()
+# Save arrays
+np.save("potential_array", phi0)
+np.save("field_array", Ez0)
+np.save("zmesh", z)
 
-warpplots = False
+# load arrays
+# Ez0 = np.load('field_array.npy')
+# phi0 = np.load('potential_array.npy')
+# z = np.load('zmesh.npy')
+
+
+# Plot potential and electric field (z-direction) on-axis.
+fig, ax = plt.subplots()
+ax.plot(z / mm, phi0 / kV)
+ax.set_xlabel("z [mm]")
+ax.set_ylabel("Potential [kV]")
+ymin, ymax = ax.get_ylim()
+for cent in shift_gap_centers:
+    left = cent - length / 2
+    right = cent + length / 2
+    ax.axvline(x=left / mm, c="gray", lw=0.7)
+    ax.axvline(x=right / mm, c="gray", lw=0.7)
+    ax.fill_between([left / mm, right / mm], [ymin, ymax], color="grey", alpha=0.5)
+ax.axhline(y=0, c="k", lw=1)
+
+fig, ax = plt.subplots()
+ax.plot(z / mm, Ez0 / E_DC)
+ax.set_xlabel("z [mm]")
+ax.set_ylabel("On-axis E-field E(x=0, y=0, z) [V/m]")
+ymin, ymax = ax.get_ylim()
+for cent in shift_gap_centers:
+    left = cent - length / 2
+    right = cent + length / 2
+    ax.axvline(x=left / mm, c="gray", lw=0.7)
+    ax.axvline(x=right / mm, c="gray", lw=0.7)
+    ax.fill_between([left / mm, right / mm], [ymin, ymax], color="grey", alpha=0.5)
+ax.axhline(y=0, c="k", lw=1)
+plt.show()
+
+
+# Warp plotting for verification that mesh and conductors were created properly.
+warpplots = True
 if warpplots:
     wp.setup()
     wp.pfzx(fill=1, filled=1)
     wp.fma()
-#
-# for i in range(steps):
-#     Ez = wp.getselfe(comp='z')[0, 0, :]
-#     Ez_array[i, :] = Ez
-#     time[i] = wp.top.time
-#     wp.step()
-#
-# np.save('Ez_gap_field_151', Ez_array)
-# np.save('zmesh', z)
-# np.save(f'time_{steps}', time)
+
+stop_here
+
+for i in range(steps):
+    Ez = wp.getselfe(comp="z")[0, 0, :]
+    Ez_array[i, :] = Ez
+    time[i] = wp.top.time
+    wp.step()
+
+np.save("Ez_gap_field_151", Ez_array)
+np.save("zmesh", z)
+np.save(f"time_{steps}", time)
+
+fig, ax = plt.subplots()
+ax.axhline(y=1, c="r", lw=1, label="Average DC Field")
+ax.plot(
+    z / mm, Ez_array[0, :] / E_DC, c="k", label=f"Time: {time_array[0]/ns:.2f} [ns]"
+)
+ax.set_xlabel("z [mm]")
+ax.set_ylabel(fr"On-axis Electric field $E_z(r=0, z,t)/E_{{dc}}$ [kV/mm]")
+ax.axvline(x=-zcenter / mm, c="gray", lw=1)
+ax.axvline(x=zcenter / mm, c="gray", lw=1)
+ax.legend()
+plt.show()
 
 # Post process
-Ez_array = np.load("Ez_gap_field_151.npy")
-time_array = np.load(f"time_{steps}.npy")
-z_array = np.load("zmesh.npy")
+# Ez_array = np.load("Ez_gap_field_151.npy")
+# time_array = np.load(f"time_{steps}.npy")
+# z_array = np.load("zmesh.npy")
 
 
-E_DC = 3.5 * kV / mm
 # fig,ax = plt.subplots()
 # ax.axhline(y=1 , c='r', lw=1, label='Average DC Field')
 # ax.plot(z/mm, Ez_array[0, :]/E_DC, c='k', label=f'Time: {time_array[0]/ns:.2f} [ns]')
