32aef19ed55f01f354f99d714c6fe0b463a8371d
Nick Valverde <nvalverde@lbl.gov>
2022-06-17 12:34:23 -0700

    Short bash script to iterate multipole options. Using python capabilities doesn't clear conductors.

--- valverde/multipole_analysis/iterate_esq.sh
+++ valverde/multipole_analysis/iterate_esq.sh
@@ -1,8 +1,26 @@
 # Assuming there is a data file with centers in it
-for x in $(cat fraction_R.txt);
-  do
-    for y in $(cat chop.txt);
-      do
-        python conductor_fieldsolve.py --scale_pole $x --rod_fraction $y
-      done
-  done
+# for x in $(cat fraction_R.txt);
+#   do
+#     for y in $(cat chop.txt);
+#       do
+#         python conductor_fieldsolve.py --scale_pole $x --rod_fraction $y
+#       done
+#   done
+
+# iterate length of ESQs
+# for x in $(cat length_scales.txt);
+#   do
+#     python conductor_fieldsolve.py --scale_length $x
+#   done
+
+# iterate Rod Radius
+# for x in $(cat rod_ratios.txt);
+#   do
+#     python conductor_fieldsolve.py --scale_pole $x
+#   done
+
+# iterate Rod Radius
+# for x in $(cat chop.txt);
+#   do
+#     python conductor_fieldsolve.py --rod_fraction $x --scale_pole 1.144
+#   done
