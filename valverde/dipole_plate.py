929e6c65e71fe90d27e90b4b961c504871061a70
Nick Valverde <nvalverde@lbl.gov>
2022-08-03 17:27:50 -0700

    Add injection of particles and a nonlinear source borrowing from Pierce diode exmaple with a Guassian modification.

--- valverde/dipole_plate.py
+++ valverde/dipole_plate.py
@@ -3,12 +3,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import warp as wp
+import scipy.constants as SC
+from warp.particles.singleparticle import TraceParticle
 
 # Define usefule constants
 mm = 1e-3
 kV = 1e3
 keV = 1e3
+uA = 1.0e-6
+Efin = 119 * keV
+Np = 10
 
+amu = SC.physical_constants["atomic mass constant energy equivalent in MeV"][0] * 1e6
+Ar_mass = 39.948 * amu
 # ------------------------------------------------------------------------------
 #    User Functions
 # ------------------------------------------------------------------------------
@@ -56,6 +63,45 @@ def getindex(mesh, value, spacing):
     return index
 
 
+def nonlinearsource():
+
+    # --- inject np particles of species electrons1
+
+    # --- Create the particles on the surface
+
+    r = 0.5 * mm
+
+    theta = 2.0 * pi * random.random(Np)
+
+    x = r * np.cos(theta)
+
+    y = r * np.sin(theta)
+
+    # Randomly pick energy using Gaussian distribution centered on Emax
+    mu = 119 * keV
+    sigma = 11.9 * kev
+    energies = np.random.normal(loc=mu, scale=sigma, size=Np)
+    vz1 = np.sqrt(2 * energies / Ar_mass) * SC.c
+
+    # --- Setup the injection arrays
+
+    wp.w3d.npgrp = Np
+
+    wp.gchange("Setpwork3d")
+
+    # --- Fill in the data. All have the same z-velocity, vz1.
+
+    wp.w3d.xt[:] = x
+
+    wp.w3d.yt[:] = y
+
+    wp.w3d.uxt[:] = 0.0
+
+    wp.w3d.uyt[:] = 0.0
+
+    wp.w3d.uzt[:] = vz1
+
+
 # ------------------------------------------------------------------------------
 #    Geoemtry and Mesh Setup
 # ------------------------------------------------------------------------------
@@ -75,16 +121,56 @@ lattice_slit_center = -2.5 * mm
 # Create mesh centering the dipole on the z-axis.
 wp.w3d.xmmin = -15.0 * mm
 wp.w3d.xmmax = 15.0 * mm
-wp.w3d.nx = 200
+wp.w3d.nx = 150
 
 wp.w3d.ymmin = -15.0 * mm
 wp.w3d.ymmax = 15.0 * mm
-wp.w3d.ny = 200
+wp.w3d.ny = 100
 
 wp.w3d.zmmin = -dipole_length / 2 - 25 * mm
 wp.w3d.zmmax = dipole_length / 2 + 20 * mm
-wp.w3d.nz = 400
+wp.w3d.nz = 300
 
+wp.w3d.l2symtry = True
+# Create ions and calculate particle characteristics
+beam = wp.Species(type=wp.Argon, charge_state=+1, name="beam")
+beam.a0 = 0.01 * mm
+beam.b0 = 0.01 * mm
+beam.ap0 = 0.0
+beam.bp0 = 0.0
+beam.ibeam = 10 * uA
+beam.vthz = 0.0
+beam.vthperp = 0.0
+beam.vbeam = np.sqrt(2 * Efin / Ar_mass) * SC.c
+wp.derivqty()
+
+# Set time step with final energy
+dz = (wp.w3d.zmmax - wp.w3d.zmmin) / wp.w3d.nz
+vzfinal = np.sqrt(2.0 * Efin / Ar_mass) * SC.c
+wp.top.dt = 0.7 * dz / vzfinal
+sim_time = (wp.w3d.zmmax - wp.w3d.zmmin) / vzfinal
+
+# Set up particle scraper to act as diagnostic. Particles will be absorbed here.
+diagnostic = wp.Box(
+    xsize=wp.top.largepos,
+    ysize=wp.top.largepos,
+    zsize=3.0 * dz,
+    voltage=0,
+    zcent=wp.w3d.zmmax - 4 * dz,
+    xcent=0.0,
+    ycent=0.0,
+)
+scraper = wp.ParticleScraper(diagnostic, lcollectlpdata=True)
+wp.top.lsavelostpart = True
+
+wp.top.inject = 1
+wp.top.npinject = 50
+wp.top.zinject = wp.w3d.zmmin
+# wp.w3d.l_inj_user_particles = True
+# wp.w3d.l_inj_user_particles_z = True
+# wp.w3d.l_inj_user_particles_v = True
+# wp.top.zinject = wp.w3d.zmmin()
+# wp.installuserparticlesinjection(nonlinearsource)
 
 wp.w3d.bound0 = wp.dirichlet
 wp.w3d.boundnz = wp.dirichlet
@@ -118,8 +204,28 @@ botplate = wp.Box(
 
 wp.installconductor(topplate)
 wp.installconductor(botplate)
+wp.installuserparticlesinjection(nonlinearsource)
 
 wp.generate()
+ions_tracked = wp.Species(type=wp.Argon, charge_state=+1, name="track")
+tracker = TraceParticle(
+    js=ions_tracked.js,
+    x=lattice_slit_center,
+    y=0.0,
+    z=wp.w3d.zmmin,
+    vx=0.0,
+    vy=0.0,
+    vz=vzfinal,
+)
+
+inject_steps = 50
+for i in range(inject_steps):
+    wp.step()
+
+# Stop injection
+wp.top.ninject = 0
+while wp.top.time < sim_time:
+    wp.step()
 
 # ------------------------------------------------------------------------------
 #    Analysis
@@ -180,7 +286,18 @@ ax.axvline(x=z[ind_dipole_end] / mm, c="k", ls="--")
 ax.legend()
 plt.show()
 
-
+fig, ax = plt.subplots()
+tracker_E = Ar_mass / 2 * pow(tracker.getvz() / SC.c, 2)
+ax.set_xlabel("z position [mm]")
+ax.set_ylabel("Kinetic Energy [keV]")
+ax.axhline(y=Efin / keV, c="r", ls="--", label=f"Ein = {Efin/keV:.2f}keV")
+ax.axhline(
+    y=tracker_E[-1] / keV, c="g", ls="--", label=f"Eout = {tracker_E[-1]/keV:.2f}keV"
+)
+ax.scatter(tracker.getz() / mm, tracker_E / keV, s=3)
+ax.plot(tracker.getz() / mm, tracker_E / keV)
+ax.legend()
+plt.show()
 # fig,ax = plt.subplots()
 # ax.set_title("Potential Along z at x=slit center (3mm)")
 # ax.set_xlabel("z [mm]")
