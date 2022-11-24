3fd474b1ba54cec76cf6a073c73b2635ce420f8c
Nick Valverde <nvalverde@lbl.gov>
2022-11-24 15:26:36 -0700

    Change file name to run the esq analysis.

--- valverde/multipole_analysis/iterate_esq.sh
+++ valverde/multipole_analysis/iterate_esq.sh
@@ -3,30 +3,30 @@
 #   do
 #     for y in $(cat chop.txt);
 #       do
-#         python conductor_fieldsolve.py --scale_pole $x --rod_fraction $y
+#         python esq_field_analysis.py --scale_pole $x --rod_fraction $y
 #       done
 #   done
 
 # iterate length of ESQs
 # for x in $(cat length_scales.txt);
 #   do
-#     python conductor_fieldsolve.py --scale_length $x
+#     python esq_field_analysis.py --scale_length $x
 #   done
 
 # iterate Rod Radius
-# for x in $(cat rod_ratios.txt);
-#   do
-#     python conductor_fieldsolve.py --scale_pole $x
-#   done
+for x in $(cat rod_ratios.txt);
+  do
+    python esq_field_analysis.py --scale_pole $x
+  done
 
 # iterate Rod Fraction
 # for x in $(cat chop.txt);
 #   do
-#     python conductor_fieldsolve.py --rod_fraction $x --scale_pole 1.145
+#     python esq_field_analysis.py --rod_fraction $x --scale_pole 1.145
 #   done
 
 # iterate Rod Voltage
 # for x in $(cat Vsets.txt);
 #   do
-#     python conductor_fieldsolve.py --voltage $x --rod_fraction 1.0
+#     python esq_field_analysis.py --voltage $x --rod_fraction 1.0
 #   done
