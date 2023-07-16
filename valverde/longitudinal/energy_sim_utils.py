40007e5119e70f6916ea68cf1744a88ca9e8ba5b
Nick Valverde <nvalverde@lbl.gov>
2023-07-16 00:59:50 -0700

    Create function plot the acceleration lattice and phasing used.

--- valverde/longitudinal/energy_sim_utils.py
+++ valverde/longitudinal/energy_sim_utils.py
@@ -6,6 +6,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.constants as SC
+
+import warp as wp
+
+# different particle masses in eV
+# amu in eV
+amu_to_eV = wp.amu * pow(SC.c, 2) / wp.echarge
+kV = 1000
+keV = 1000
+MHz = 1e6
+cm = 1e-2
+mm = 1e-3
+um = 1e-6
+us = 1e-6
+ns = 1e-9  # nanoseconds
+mA = 1e-3
+uA = 1e-6
+twopi = 2 * np.pi
 
 
 def make_dist_plot(
@@ -43,3 +61,40 @@ def make_dist_plot(
         )
 
     return g
+
+
+def make_lattice_plot(
+    zmesh, Efield, gap_centers, phi_s, gap_voltage, gap_width, Fcup_dist
+):
+    "Plot the acceleration lattice with some labeling."
+
+    E_DC = gap_voltage / gap_width
+    Ng = len(gap_centers)
+
+    fig, ax = plt.subplots()
+    ax.set_title("Accel. Lattice with Applied Field at t=0", fontsize="large")
+    ax.set_xlabel("z [mm]", fontsize="large")
+    ax.set_ylabel(r"$E(r=0, z)/E_{DC}$ [kV/mm]", fontsize="large")
+    ax.plot(zmesh / mm, Efield / E_DC)
+    ax.axvline(gap_centers[0] / mm, c="grey", lw=1, ls="--", label="Gap Center")
+    if Ng > 1:
+        for i, cent in enumerate(gap_centers[1:]):
+            ax.axvline(cent / mm, c="grey", lw=1, ls="--")
+
+    ax.axvline(
+        (gap_centers[-1] + gap_width / 2 + Fcup_dist) / mm,
+        c="r",
+        lw=1,
+        ls="dashdot",
+        label="Fcup",
+    )
+    ax.legend()
+    plt.tight_layout()
+
+    fig, ax = plt.subplots()
+    ax.yaxis.grid(True)
+    ax.set_title("Synchronous Phase at Gap")
+    ax.set_xlabel(r"$N_g$")
+    ax.set_ylabel(r"$\phi_s$ [deg]")
+    ax.scatter([i + 1 for i in range(Ng)], phi_s / np.pi * 180)
+    plt.tight_layout()
